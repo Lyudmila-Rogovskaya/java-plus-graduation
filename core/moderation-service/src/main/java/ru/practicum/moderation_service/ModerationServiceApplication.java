b7b0f7c6eda449e5e196dae4246a6fbf8c73a2ab
Lyudmila Rogovskaya <lyussenok@yandex.ru>
2026-04-05 13:23:49 +0700

    Дипломный проект - этап 2 (настройка сервисов 9)

--- core/moderation-service/src/main/java/ru/practicum/moderation_service/ModerationServiceApplication.java
+++ core/moderation-service/src/main/java/ru/practicum/moderation_service/ModerationServiceApplication.java
@@ -3,11 +3,11 @@ package ru.practicum.moderation_service;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import org.springframework.cloud.openfeign.EnableFeignClients;
+//import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
 @EnableDiscoveryClient
-@EnableFeignClients
+//@EnableFeignClients
 public class ModerationServiceApplication {
 
     public static void main(String[] args) {
