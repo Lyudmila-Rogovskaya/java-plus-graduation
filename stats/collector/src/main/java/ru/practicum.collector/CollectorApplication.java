75cfc89b3aed0dc198f1606e2426f11441892e7b
Lyudmila Rogovskaya <lyussenok@yandex.ru>
2026-04-12 20:23:20 +0700

    Дипломный проект - этап 3 (настройка сервисов 5)

--- stats/collector/src/main/java/ru/practicum.collector/CollectorApplication.java
+++ stats/collector/src/main/java/ru/practicum.collector/CollectorApplication.java
@@ -2,10 +2,12 @@ package ru.practicum.collector;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
 @EnableDiscoveryClient
+@ConfigurationPropertiesScan
 public class CollectorApplication {
 
     public static void main(String[] args) {
